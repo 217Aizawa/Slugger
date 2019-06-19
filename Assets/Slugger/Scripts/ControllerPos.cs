6d9dab7a79a13bbd2e8b2c3a258fc2482a350c6f
217Aizawa <21711001ha@tama.ac.jp>
2019-06-19 21:45:47 +0900

    Fixed Camera Height

--- Assets/Slugger/Scripts/ControllerPos.cs
+++ Assets/Slugger/Scripts/ControllerPos.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class ControllerPos : MonoBehaviour
 {
+    //GameObject controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //controller = GameObject.FindGameObjectWithTag("Controller");
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(this.transform.position);
     }
 }
---
+++ Assets/Slugger/Scripts/FixCameraPosY.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR;
+using UnityEngine;
+
+
+public class FixCameraPosY : MonoBehaviour
+{
+    Vector3 cameraPos ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cameraPos = new Vector3(this.transform.position.x, 0.0f, this.transform.position.z);
+        cameraPos = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localPosition = cameraPos;
+        
+    }
+}
