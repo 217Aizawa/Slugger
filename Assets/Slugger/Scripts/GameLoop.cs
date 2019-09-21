cc3c721b0f1fa2b6951788e5b2f28a3befafaa70
217Aizawa <21711001ha@tama.ac.jp>
2019-09-21 14:50:37 +0900

    save

--- Assets/Slugger/Scripts/GameLoop.cs
+++ Assets/Slugger/Scripts/GameLoop.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameLoop : MonoBehaviour
 {
+    public enum GameState { free, play, throwing, batting};
+
+    public GameState gameState = GameState.free;
+
+    public BatController batController;
+    public CameraController camereController;
+    public ThrowController throwController;
+    public PhsController phsController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,23 @@ public class GameLoop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch(gameState)
+        {
+            case GameState.free:
+
+                break;
+
+            case GameState.play:
+
+                break;
+
+            case GameState.throwing:
+
+                break;
+
+            case GameState.batting:
+
+                break;
+        }
     }
 }
--- Assets/Slugger/Scripts/ThrowController.cs
+++ Assets/Slugger/Scripts/ThrowController.cs
@@ -4,56 +4,46 @@ using UnityEngine;
 
 public class ThrowController : MonoBehaviour
 {
-    private Vector3 throwPos;//投球位置
-    private Vector3 target;//目標
-    private int speed = 5;
-    private bool timeCheck = false;
-    public GameObject ball;
+    Vector3 throwPos;//投球位置
+    Vector3 target;//目標
+    bool timeCheck = false;
     float time = 0;
-    public GameObject nextBall;//次に生成するボール
-    
+    int speed = 5;
+    public GameObject ball;
+
     void Start()
     {
-        throwPos = ball.transform.position;//投球位置をボールのある位置に設定する。
+        throwPos = ball.transform.position;
     }
 
     void Update()
     {
-        //Rigidbody rig = ball.GetComponent<Rigidbody>();
         TimeCounter();
-        if (Input.GetKey(KeyCode.Space))
+
+        if (Input.GetKey(KeyCode.Space))    //条件文をGameStateで書く
         {
             timeCheck = true;
             Throw();
         }
+
         if(3.0f <= time)
         {
-            //ball.SetActive(false); クローンまで非表示なってしまう
-            //Destroy(ball);
-            //既存のボールを投球後にInstantiateでボールを生成し、投げる。を繰り返す。
-            //ball = Instantiate(ball, throwPos, Quaternion.identity);
             timeCheck = !timeCheck;
-            /*ball.transform.position = throwPos;//投球位置に戻す
-            rig.useGravity = false;
-            rig.isKinematic = true;*/
             BallReset();
         }
     }
-    public void TimeCounter()
+
+    void TimeCounter()
     {
         Debug.Log("Time" + time);
+
         if (timeCheck)
-        {
             time += Time.deltaTime;
-            //Debug.Log("Time" + time);
-        }
         else
-        {
             time = 0;
-        }
     }
 
-    public void Throw()//投球関数
+    private void Throw()//投球関数
     {
         Rigidbody rb = ball.GetComponent<Rigidbody>();
         rb.useGravity = true;
@@ -61,7 +51,7 @@ public class ThrowController : MonoBehaviour
         rb.velocity = transform.forward * speed;//速度を加算
     }
 
-    private void BallReset()//投球後にボールの情報をリセットする
+    private void BallReset()//ボール情報をリセットする
     {
         ball.transform.position = throwPos;
         Rigidbody rig = ball.GetComponent<Rigidbody>();
