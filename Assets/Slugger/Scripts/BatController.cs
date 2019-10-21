7f2eff5f539b0c40dfdd4fd7f1458b5134a673a9
217Aizawa <21711001ha@tama.ac.jp>
2019-10-21 18:03:07 +0900

    save

--- Assets/Slugger/Scripts/BatController.cs
+++ Assets/Slugger/Scripts/BatController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BatController : MonoBehaviour
 {
+    public GameObject Bat;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class BatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("BatPos" + Bat.transform.position);
     }
 }
