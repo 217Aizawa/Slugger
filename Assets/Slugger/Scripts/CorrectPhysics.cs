3d2998c111cef49aa336ee08764f4c74e37140cb
217Aizawa <21711001ha@tama.ac.jp>
2019-10-26 14:04:31 +0900

    deleted old bat

--- Assets/Slugger/Scripts/CorrectPhysics.cs
+++ Assets/Slugger/Scripts/CorrectPhysics.cs
@@ -26,14 +26,24 @@ public class CorrectPhysics : MonoBehaviour
         }
         float dt = Time.time - t0;//経過時間
         transform.position = p0 + v0 * dt - 0.5f * Vector3.up  * 9.8f * dt * dt;//物理演算
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Disable();
     }
 
     public void Disable()//kinemativ無効
     {
         rb.isKinematic = isKinematic;
         isEanbled = false;
+        //バットと衝突した瞬間に新たな方向に速度を加える
+        //rb.velocity = new Vector3();
+        Debug.Log("OnCollisionEnter");
     }
 
+    //Throw関数ないから呼び出す
     public void Enable(Vector3 v)//kinematic有効
     {
         rb = GetComponent<Rigidbody>();//基の状態を記憶
@@ -43,7 +53,7 @@ public class CorrectPhysics : MonoBehaviour
         v0 = v;
         t0 = Time.time;//ゲームスタートからの経過時間
         isEanbled = true;
-
+        Debug.Log("Called Enable");
     }
 
     
