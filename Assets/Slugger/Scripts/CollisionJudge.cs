f61991228ef7e2185da9b3ff32b3e82f6294dda7
217Aizawa <21711001ha@tama.ac.jp>
2019-11-04 21:06:46 +0900

    add some code on CollisionJudge.cs

--- Assets/Slugger/Scripts/CollisionJudge.cs
+++ Assets/Slugger/Scripts/CollisionJudge.cs
@@ -6,26 +6,45 @@ public class CollisionJudge : MonoBehaviour
 {
     GameObject bat;
     GameObject ball;
- 
+    float batRad;//バット半径
+    float ballDia;//ボールの直径
+    float collisonDist;
+    float dist;
+    Vector3 batPos;
+    Vector3 ballPos;
+
     // Start is called before the first frame update
     void Start()
     {
         bat = this.GetComponent<GameObject>();
         ball = GameObject.Find("ball");
+
+        batRad = 0.0355f;//バットのColliderから参照
+        ballDia = 0.0723f;
+        collisonDist = batRad + ballDia;
     }
 
     // Update is called once per frame
     void FixeedUpdate()
     {
+        dist = Vector3.Distance(batPos, ballPos);
+        Debug.Log("dist" + dist);
+        //もしも、バットとボールの距離が batRad + ballDia 以下ならば...
+        if (dist > collisonDist)
+        {
+            this.gameObject.GetComponent<CorrectPhysics>().Disable();
+            Debug.Log("collison");
+        }
+
         /*if (Physics.Raycast(transform.position, transform.forward, 1))
         {
             this.gameObject.GetComponent<CorrectPhysics>().Disable();
             Debug.Log("Ray hit");
         }*/
-        
+
     }
 
-    private void OnCollisionEnter(Collision other)
+    /*private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.tag == "Bat")
         {
@@ -33,5 +52,5 @@ public class CollisionJudge : MonoBehaviour
             this.gameObject.GetComponent<CorrectPhysics>().Disable();
             Debug.Log("Called Disable");
         }
-    }
+    }*/
 }
