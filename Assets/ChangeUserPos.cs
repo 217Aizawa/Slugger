bb88c8da2f55f22a4c83c911bd7f4717bafbf8c5
217Aizawa <21711001ha@tama.ac.jp>
2020-04-13 08:04:13 +0900

    add ChangeUserPos.cs

---
+++ Assets/ChangeUserPos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeUserPos : MonoBehaviour
+{
+    GameObject user;
+    Vector3 userPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        user = this.gameObject;
+        userPos = user.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.L))//右座席用座標
+        {
+            user.transform.position = new Vector3(0.5f, userPos.y, userPos.z);
+            Debug.Log("Left");
+        }
+        if (Input.GetKey(KeyCode.R))//左座席用座標
+        {
+            user.transform.position = new Vector3(-0.5f, userPos.y, userPos.z);
+            Debug.Log("Right");
+        }
+    }
+}
