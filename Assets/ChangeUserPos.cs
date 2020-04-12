28f1fa426b4397e1bee6340cee9414c5363d692f
217Aizawa <21711001ha@tama.ac.jp>
2020-04-13 08:34:14 +0900

    fixed ChangeUserPos.cs

--- Assets/ChangeUserPos.cs
+++ Assets/ChangeUserPos.cs
@@ -7,25 +7,23 @@ public class ChangeUserPos : MonoBehaviour
     GameObject user;
     Vector3 userPos;
 
-    // Start is called before the first frame update
     void Start()
     {
         user = this.gameObject;
         userPos = user.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.L))//右座席用座標
+        //右座席用座標
+        if (Input.GetKey(KeyCode.L))                                                        // || OVRInput.GetDown(OVRInput.Button.Three)) //Three = Xボタン
         {
             user.transform.position = new Vector3(0.5f, userPos.y, userPos.z);
-            Debug.Log("Left");
         }
-        if (Input.GetKey(KeyCode.R))//左座席用座標
+        //左座席用座標
+        if (Input.GetKey(KeyCode.R))                                                        // || OVRInput.GetDown(OVRInput.Button.Four)) //Four = Yボタン
         {
             user.transform.position = new Vector3(-0.5f, userPos.y, userPos.z);
-            Debug.Log("Right");
         }
     }
 }
