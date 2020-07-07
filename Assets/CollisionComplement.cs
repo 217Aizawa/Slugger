cac01c020b98923cb9bc24931e7b395a2414d05d
217Aizaw <21711001ha@tama.ac.jp>
2020-07-08 01:20:46 +0900

    before problem resolve

--- Assets/CollisionComplement.cs
+++ Assets/CollisionComplement.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class CollisionComplement : MonoBehaviour
 {
 
-    //　剣元1
+    //グリップ
     public Transform startPosition;
-    //　剣元2
     public Transform startPosition2;
-    //　剣先1
+    //ヘッド
     public Transform endPosition;
-    //　剣先2
     public Transform endPosition2;
 
     //　メッシュ
@@ -22,13 +20,17 @@ public class CollisionComplement : MonoBehaviour
     public List<int> tempTriangles = new List<int>();
     //　軌跡の表示のオン・オフフラグ
     private bool isSwordCollider = false;
-    //　前フレームの剣元の位置
+    //　前フレームのグリップの位置
     private Vector3 oldStartPos;
-    //　前フレームの剣先の位置
+    //　前フレームのヘッドの位置
     private Vector3 oldEndPos;
 
     private MeshCollider meshCollider;
     private MeshFilter meshFilter;
+
+    float speed;
+    Vector3 prevPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -43,8 +45,57 @@ public class CollisionComplement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        speed = ((transform.position - prevPos) / Time.deltaTime).magnitude;
+
+        if(2 < speed)
+        {
+            CreateMesh();
+        }
+
+        prevPos = transform.position;
+    }
+
+    //バットの軌跡作成メソッド
+    void CreateMesh()
+    {
+        //mesh.Clear();
+
+        //　頂点以外のリストのクリア
+        verticesLists.Clear();
+        tempTriangles.Clear();
+
+        verticesLists.AddRange(new Vector3[] {
+        oldStartPos, oldEndPos,
+        startPosition.position, endPosition.position,
+        startPosition2.position, endPosition2.position
+    });
+
+        //　本当なら全面作らなければいけないがCovexにチェックを入れると勝手に作ってくれる
+        tempTriangles.AddRange(new int[]{
+        0, 1, 2,
+        2, 1, 3,
+        2, 3, 4,
+        4, 3, 5,
+		/*
+		0, 2, 4,
+		5, 1, 3
+		*/
+	});
+
+        mesh.vertices = verticesLists.ToArray();
+        mesh.triangles = tempTriangles.ToArray();
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        //　メッシュコライダの再有効化
+        meshCollider.enabled = false;
+        meshCollider.enabled = true;
+
+        oldStartPos = startPosition.position;
+        oldEndPos = endPosition.position;
+
     }
 }
--- Assets/Slugger/Scripts/BatController.cs
+++ Assets/Slugger/Scripts/BatController.cs
@@ -4,75 +4,51 @@ using UnityEngine;
 
 public class BatController : MonoBehaviour
 {
-    public static GameObject bat;
+    /*Vector3 latestPos;//過去位置
+    Vector3 speed;*/
     public GameObject emptyBat;
     GameObject model;
-
+    public static GameObject bat;
     Rigidbody batRb;
 
-    public bool isBat = false;//バットの軌道表示
-    //bool swing = false;
-
-    //自動スイング用
-    //Vector3 startPos = new Vector3(-0.4f ,0.8f ,19.8f);
-    //Vector3 endPos;
-
-    //ローカルな現在位置
-    Vector3 batGripLocal;
-    Vector3 batHeadLocal;
-    Vector3 batGripLocal2;
-    Vector3 batHeadLocal2;
-    Vector3 batDirLocal;
+    Vector3 pos;
+    Vector3 latestPos;//過去位置
 
     //現在位置
-    public static Vector3 batHead;
-    public static Vector3 batGrip;
-    public static Vector3 batHead2;
-    public static Vector3 batGrip2;
-    public static Vector3 batDir;
-    Vector3 nowPos;
-
+    Vector3 batGripLocal;
+    Vector3 batDirLocal;
+    Vector3 batHeadLocal;
     //過去位置
-    Vector3 prevBatHead;
     Vector3 prevBatGrip;
     Vector3 prevBatDir;
-    Vector3 latestPos;
+    Vector3 prevBatHead;
 
+    //自動スイング用
+    Vector3 startPos = new Vector3(-0.4f ,0.8f ,19.8f);
+    Vector3 endPos;
+
+    public bool isBat = false;//バットの軌道表示
+
+    public static Vector3 batHead;
+    public static Vector3 batGrip;
+    public static Vector3 batDir;
     public static Vector3 swingSpeed;
 
-    
-    private Mesh mesh;//　メッシュ
-    public List<Vector3> verticesLists = new List<Vector3>();//　頂点リスト
-    public List<int> tempTriangles = new List<int>();//　三角形のリスト
-    private bool isSwordCollider = false;//　軌跡の表示のオン・オフフラグ
-    private Vector3 oldStartPos;//　前フレームの剣元の位置
-    private Vector3 oldEndPos;//　前フレームの剣先の位置
-
-    private MeshCollider meshCollider;
-    private MeshFilter meshFilter;
+    //bool swing = false;
 
     void Start()
     {
+        pos = this.gameObject.transform.position;
         batRb = GetComponent<Rigidbody>();
         batDirLocal = new Vector3(0, 0.9f, 0);//バットの長さ=90cm
         batGripLocal = new Vector3(0, -0.45f, 0);
         batHeadLocal = new Vector3(0, 0.45f, 0);
-        batGripLocal2 = new Vector3(0, -0.45f, 0);
-        batHeadLocal2 = new Vector3(0, 0.45f, 0);
         swingSpeed = new Vector3(0, 0, 0);
-
-        mesh = GetComponent<MeshFilter>().mesh;
-        meshCollider = GetComponent<MeshCollider>();
-        meshCollider.convex = true;//　他の凸コライダと衝突有効にする
-        meshCollider.isTrigger = true;//　物理的に当たらないようにする
-        meshCollider.sharedMesh = mesh;//　メッシュコライダに自作メッシュを設定
-
     }
 
     void FixedUpdate()
     {
-        nowPos = this.gameObject.transform.position;
-        swingSpeed = ((nowPos - latestPos) / Time.deltaTime);
+        swingSpeed = ((transform.position - latestPos) / Time.deltaTime);
 
         batDir = transform.TransformDirection(batDirLocal);//向きだけ変換する
         batGrip = transform.TransformPoint(batGripLocal);//グリップ位置
@@ -80,13 +56,9 @@ public class BatController : MonoBehaviour
 
         //Debug.Log("World :" + batDir + " Local :" + batDirLocal);
         //Debug.Log("World :" + batGrip + " Local :" + batGripLocal);
+
         //Debug.Log("magnitude" + swingSpeed.magnitude);
 
-        if(2 < swingSpeed.magnitude)
-        {
-            CreateMesh();
-            Debug.Log("CreateMesh called");
-        }
         if (5 < swingSpeed.magnitude && isBat)
         {
             model = Instantiate(emptyBat, transform.position,transform.rotation);
@@ -106,52 +78,10 @@ public class BatController : MonoBehaviour
         */
 
         /**********************過去位置**************************/
-        prevBatDir = batDir;
-        prevBatGrip = batGrip;
-        prevBatHead = batHead;
+        //prevBatDir = batDir;
+        //prevBatGrip = batGrip;
+        //prevBatHead = batHead;
         latestPos = transform.position;
         /********************************************************/
-    }
-
-    void CreateMesh()
-    {
-        mesh.Clear();
-
-        //　頂点以外のリストのクリア
-        verticesLists.Clear();
-        tempTriangles.Clear();
-
-        verticesLists.AddRange(new Vector3[] {
-        prevBatHead, prevBatGrip,
-        batHead, batGrip,
-        batHead2, batGrip2
-    });
-
-        //　本当なら全面作らなければいけないがCovexにチェックを入れると勝手に作ってくれる
-        tempTriangles.AddRange(new int[]{
-        0, 1, 2,
-        2, 1, 3,
-        2, 3, 4,
-        4, 3, 5,
-		/*
-		0, 2, 4,
-		5, 1, 3
-		*/
-	});
-
-        mesh.vertices = verticesLists.ToArray();
-        mesh.triangles = tempTriangles.ToArray();
-
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-
-        //　メッシュコライダの再有効化
-
-        meshCollider.enabled = false;
-        meshCollider.enabled = true;
-
-        //oldStartPos = startPosition.position;
-        //oldEndPos = endPosition.position;
-
     }
 }
