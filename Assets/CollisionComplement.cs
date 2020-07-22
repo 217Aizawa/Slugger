516ed1b36a186d33fa3beb84fecbf13aaa6fce3c
217Aizaw <21711001ha@tama.ac.jp>
2020-07-22 15:02:26 +0900

    change if statement fot CollisionComp.cs

--- Assets/CollisionComplement.cs
+++ Assets/CollisionComplement.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class CollisionComplement : MonoBehaviour
 {
 
-    //グリップ
-    public Transform startPosition;
-    public Transform startPosition2;
-    //ヘッド
-    public Transform endPosition;
-    public Transform endPosition2;
+    //グリップ位置
+    public Transform gripPosition;
+    public Transform gripPosition2;
+    //ヘッド位置
+    public Transform headPosition;
+    public Transform headPosition2;
 
     private Mesh mesh;
     //　頂点リスト
@@ -18,9 +18,9 @@ public class CollisionComplement : MonoBehaviour
     //　三角形のリスト
     public List<int> tempTriangles = new List<int>();
     //　前フレームのグリップの位置
-    private Vector3 oldStartPos;
+    private Vector3 oldGripPos;
     //　前フレームのヘッドの位置
-    private Vector3 oldEndPos;
+    private Vector3 oldheadPos;
 
     private MeshCollider meshCollider;
     private MeshFilter meshFilter;
@@ -28,16 +28,16 @@ public class CollisionComplement : MonoBehaviour
     Vector3 prevPos;
 
     //　軌跡の表示のオン・オフフラグ
-    private bool isSwordCollider = false;
+    private bool isBatCollider = false;
     // Start is called before the first frame update
     void Start()
     {
         mesh = GetComponent<MeshFilter>().mesh;
         meshCollider = GetComponent<MeshCollider>();
         //　他の凸コライダと衝突有効にする
-        meshCollider.convex = true;
+        //meshCollider.convex = true;
         //　物理的に当たらないようにする
-        meshCollider.isTrigger = true;
+        //meshCollider.isTrigger = true;
         //　メッシュコライダに自作メッシュを設定
         meshCollider.sharedMesh = mesh;
     }
@@ -46,9 +46,10 @@ public class CollisionComplement : MonoBehaviour
     void FixedUpdate()
     {
         speed = ((transform.position - prevPos) / Time.deltaTime).magnitude;
-        Debug.Log("speed" + speed);
-        if(2 < speed)
+        //Debug.Log("speed" + speed);
+        if(2 < speed && speed < 1000)
         {
+            Debug.Log("create mesh called");
             CreateMesh();
         }
 
@@ -62,23 +63,34 @@ public class CollisionComplement : MonoBehaviour
         //　リストのクリア
         verticesLists.Clear();
         tempTriangles.Clear();
-
+        
         verticesLists.AddRange(new Vector3[] {
-        oldStartPos, oldEndPos,
-        startPosition.position, endPosition.position,
-        startPosition2.position, endPosition2.position
+        oldGripPos, oldheadPos,
+        gripPosition.position, headPosition.position,
+        gripPosition2.position, headPosition2.position
     });
-
-        //　本当なら全面作らなければいけないがCovexにチェックを入れると勝手に作ってくれる　頂点を結ぶ順番
+        
+        //　頂点を結ぶ順番
         tempTriangles.AddRange(new int[]{
         0, 1, 2,
         2, 1, 3,
         2, 3, 4,
         4, 3, 5,
-		/*
-		0, 2, 4,
-		5, 1, 3
-		*/
+        //追加分
+        0, 1, 4,
+        4, 1, 5,
+        0, 2, 4,
+        5, 1, 3,
+		/*//足りない分を追加する　convexは使わない
+        2,3,4//側面
+        4,3,5//側面2
+
+        2,3,1
+        4,5,1
+        
+		0, 2, 4,//新旧grip
+		5, 1, 3*///新旧head
+		
 	});
         
         mesh.vertices = verticesLists.ToArray();
@@ -91,8 +103,8 @@ public class CollisionComplement : MonoBehaviour
         meshCollider.enabled = false;
         meshCollider.enabled = true;
 
-        oldStartPos = startPosition.position;
-        oldEndPos = endPosition.position;
+        oldGripPos = gripPosition.position;
+        oldheadPos = headPosition.position;
 
     }
 }
---
+++ Assets/CubeMesh.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMesh : MonoBehaviour
+{
+    private Mesh mesh;
+    private MeshCollider meshCollider;
+    private MeshFilter meshFilter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        meshCollider = GetComponent<MeshCollider>();
+        meshCollider.sharedMesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mesh.Clear();
+    }
+}
