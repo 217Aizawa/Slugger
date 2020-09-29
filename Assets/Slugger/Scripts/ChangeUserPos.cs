434a95bb232942f84af2b5fd163bd4f373cf886c
217Aizaw <21711001ha@tama.ac.jp>
2020-09-29 14:34:26 +0900

    save

--- Assets/Slugger/Scripts/ChangeUserPos.cs
+++ Assets/Slugger/Scripts/ChangeUserPos.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ChangeUserPos : MonoBehaviour
 {
+    //打席切り替え用プログラム
     GameObject user;
     Vector3 userPos;
 
@@ -17,14 +18,14 @@ public class ChangeUserPos : MonoBehaviour
     {
         //右座席用座標
         if (Input.GetKey(KeyCode.R) ||
-            OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch)) //Xボタン
+            OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch)) //Aボタン
         {
             Debug.Log("");
             user.transform.position = new Vector3(0.5f, userPos.y, userPos.z);
         }
         //左座席用座標
         if (Input.GetKey(KeyCode.L) ||
-            OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch)) //Yボタン
+            OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch)) //Bボタン
         {
             user.transform.position = new Vector3(-0.5f, userPos.y, userPos.z);
         }
